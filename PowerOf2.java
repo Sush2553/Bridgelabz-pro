cc3989c36704e2eb6134b060ce39e806dd5fff55
Admin <sushantpatwari1234@gmail.com>
2019-03-07 17:06:28 +0530

    added all

--- PowerOf2.java
+++ PowerOf2.java
@@ -1,9 +1,11 @@
+package com.bridgelabz.functional;
 
 
 public class PowerOf2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Sush");
+		// TODO Auto-generated method stub
+		System.out.println("hhhhhhhiiii");
 	}
 
 }
